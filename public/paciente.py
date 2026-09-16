3a91c9958a42facf76555788d7d5e49996feb727
Yasmin Macena <yasmacx@gmail.com>
2026-09-16 19:51:20 -0300

    mais funcionalidades

--- public/paciente.py
+++ public/paciente.py
@@ -1,28 +1,39 @@
-print('Cadastro do Paciente')
-
 def painel_paciente():
-    print('='* 10)
+    print('='* 30)
     print('Painel de Controle')
-    print('='* 10)
+    print('='* 30)
     print('\n')
 
-    escolha = int(input('Digite sua opção:'))
-    while true:
-        if escolha == 1:
-            cadastrar_paciente()
-            break
-        elif escolha == 2:
-            agendar_consulta()
-            break
-        elif escolha == 3:
-            agendamento_paciente()
-            break
-        else:
-            print('Opção invalida! Tente novamente.')
+    escolha = int(input('Digite sua opção:\n'))
+    while True:
+        try:
+            if escolha == 1:
+                print('Cadastrar Paciente\n')
+                cadastrar_paciente()
+                break
+            elif escolha == 2:
+                print('Agendar consulta\n')
+                agendar_consulta()
+                break
+            elif escolha == 3:
+                print('Agenda do paciente\n')
+                agendamento_paciente()
+                break
+            else:
+                print('Opção invalida! Tente novamente.')
+        except ValueError:
+            print('Por favor, digite apenas números inteiros.\n')
+
 
 def cadastrar_paciente():
-    cpf = int(input = ('Digite seu CPF:'))
+    cpf = int(input('Digite seu CPF:'))
     nome = input = ('Digite seu nome:')
-    nascimento = int(input = ('Digite sua data de nascimento:'))
+    nascimento = int(input('Digite sua data de nascimento:'))
 
-painel_paciente()
+def agendar_consulta():
+    print('='* 30)
+def agendamento_paciente():
+    print('='* 30)
+
+if __name__ == "__main__":
+    painel_paciente()
